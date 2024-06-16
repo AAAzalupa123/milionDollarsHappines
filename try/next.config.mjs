4ee431bcf51eaa51849013d78fc4e85418817379
NeZalupaXXX <ntnpipito@gmail.com>
2024-06-16 16:53:34 +0300

    next

--- try/next.config.mjs
+++ try/next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { NextConfig } from 'next';
+
+const nextConfig = {
+  output: 'export',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+};
 
 export default nextConfig;
